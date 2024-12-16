d2368731003da3e41ba235fe2ea4a7ec44faf9b7
SumiyaMim <sumiyaislam1093@gmail.com>
2024-12-16 23:34:54 +0600

    add checkout modal

--- react-version/src/App.jsx
+++ react-version/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { FaStar, FaStarHalfAlt, FaRegStar } from 'react-icons/fa';
 import { FaMinus, FaPlus } from "react-icons/fa6";
 import { HiOutlineHeart } from "react-icons/hi";
@@ -12,7 +12,9 @@ function App() {
   const [image, setImage] = useState(purpleImage);
   const [selectedSize, setSelectedSize] = useState(null);
   const [quantity, setQuantity] = useState(0);
-  const [cartItems, setCartItems] = useState(0);
+  const [cartItems, setCartItems] = useState([]);
+  const [checkoutModal, setCheckoutModal] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
   // handle band color
   const handleColorChange = (event) => {
@@ -58,12 +60,74 @@ function App() {
     }
   };
 
+  // get cart items from localStorage on page load
+  useEffect(() => {
+    const savedCart = JSON.parse(localStorage.getItem("cart")) || [];
+    setCartItems(savedCart);
+  }, []);
+
   // handle add to cart 
   const handleCart = () => {
     if (quantity > 0) {
-      setCartItems(cartItems + quantity);
+      const productDetails = {
+        name: "Classy Modern Smart Watch",
+        size: selectedSize ? selectedSize : "N/A",
+        color: selectedColor,
+        price: selectedSize === "S" ? 69 : selectedSize === "M" ? 79 : selectedSize === "L" ? 89 : 99,
+        image: image,
+        quantity: quantity,
+      };
+
+      // get product details in from
+      let cart = JSON.parse(localStorage.getItem("cart")) || [];
+
+      // check the color and size
+      const existingItem= cart.findIndex(
+        item => item.color === productDetails.color && item.size === productDetails.size
+      );
+
+      // update quantity
+      if (existingItem !== -1) {
+        cart[existingItem].quantity += quantity;
+      } else {
+        // add new product
+        cart.push(productDetails);
+      }
+
+      // store updated product details to localStorage
+      localStorage.setItem("cart", JSON.stringify(cart));
+
+      // update cart items state 
+      setCartItems(cart);
+      
+      // reset quantity
       setQuantity(0); 
-    }  
+    }
+  };
+
+  // calculate total quantity and total price
+  const totalQuantity = cartItems.reduce((acc, item) => acc + item.quantity, 0);
+  const totalPrice = cartItems.reduce((acc, item) => acc + item.price * item.quantity, 0).toFixed(2);
+
+  // handle toggle modal 
+  const toggleModal = () => {
+    setCheckoutModal(!checkoutModal);
+  };
+
+  // handle checkout open modal button
+  const openModal = () => {
+    setIsModalOpen(true);
+  };
+
+  // handle checkout close modal
+  const closeModal = () => {
+    setIsModalOpen(false);
+
+    // clear cart data in localStorage
+    setCartItems([]);
+    localStorage.setItem("cart", JSON.stringify([]));
+
+    setCheckoutModal(!checkoutModal);
   };
 
   return (
@@ -276,20 +340,112 @@ function App() {
       </div>
 
       {/* Checkout Section */}
-      <div id="checkout-section" className={`flex justify-center items-center mt-6 mb-16 ${cartItems === 0 ? 'hidden' : ''}`}>
-      {cartItems > 0 && (
-        <button
-          id="checkout-button"
-          className="block flex items-center justify-center space-x-3 px-6 py-3 bg-[#FFBB5A] font-bold text-[#364A63] rounded-full shadow-[0_8px_30px_rgb(0,0,0,0.12)] hover:bg-[#f5b254]"
-          type="button"
+      <div id="checkout-section" className={`flex justify-center items-center mt-6 mb-16 ${cartItems.length === 0 ? 'hidden' : ''}`}>
+        {cartItems.length > 0 && (
+          <button
+            id="checkout-button"
+            className="block flex items-center justify-center space-x-3 px-6 py-3 bg-[#FFBB5A] font-bold text-[#364A63] rounded-full shadow-[0_8px_30px_rgb(0,0,0,0.12)] hover:bg-[#f5b254]"
+            type="button"
+            onClick={toggleModal}
+          >
+            <span>Checkout</span>
+            <span id="item-count" className="flex items-center justify-center w-6 h-6 bg-white text-sm rounded-md">
+              {cartItems.length}
+            </span>
+          </button>
+        )}
+      </div>
+
+      {/* Checkout Modal */}
+      {checkoutModal && (
+        <div
+          id="popup-modal"
+          className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50"
         >
-          <span>Checkout</span>
-          <span id="item-count" className="flex items-center justify-center w-6 h-6 bg-white text-sm rounded-md">
-            {cartItems}
-          </span>
-        </button>
+          <div className="relative p-4 w-[90%] md:w-full max-w-2xl max-h-full bg-white rounded-lg shadow dark:bg-gray-700 overflow-y-auto">
+            <div className="p-4 md:p-10">
+              <h3 className="mb-2 text-xl font-bold text-[#364A63]">
+                Your Cart
+              </h3>
+
+              {/* Cart Table */}
+              <table className="w-full text-left text-sm text-[#8091A7] mb-6">
+                <thead>
+                  <tr className="border-b text-[#8091A7]">
+                    <th className="pr-4 py-3 font-normal">Item</th>
+                    <th className="px-4 py-3 text-center font-normal">Color</th>
+                    <th className="px-4 py-3 text-center font-normal">Size</th>
+                    <th className="px-4 py-3 text-center font-normal">Qnt</th>
+                    <th className="text-right py-3 font-normal">Price</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {cartItems.map((item, index) => (
+                      <tr className="text-[#364A63] border-b" key={index}>
+                        <td className="pr-4 py-3">
+                          <div className="flex flex-col md:flex-row items-center">
+                            <img className="w-12 h-12 rounded" src={item.image} alt="Item" />
+                            <div className="ml-2">
+                              <p>{item.name}</p>
+                            </div>
+                          </div>
+                        </td>
+                        <td className="px-4 py-3 text-center">{item.color.charAt(0).toUpperCase() + item.color.slice(1)}</td>
+                        <td className="px-4 py-3 text-center font-semibold">{item.size}</td>
+                        <td className="px-4 py-3 text-center font-semibold">{item.quantity}</td>
+                        <td className="text-right py-3 font-semibold">${(item.price * item.quantity).toFixed(2)}</td>
+                      </tr>
+                  ))}
+                    
+                  <tr className="text-[#364A63]">
+                    <td className="pr-4 py-5 text-lg font-semibold">Total</td>
+                    <td className="px-4 py-5 text-center"></td>
+                    <td className="px-4 py-5 text-center"></td>
+                    <td className="px-4 py-5 text-center font-semibold">{totalQuantity}</td>
+                    <td className="text-right py-5 text-lg font-semibold">${totalPrice}</td>
+                  </tr>
+                </tbody>
+              </table>
+
+              <div className="flex justify-end items-center space-x-5">
+                <button
+                  onClick={toggleModal}
+                  className="py-2.5 px-5 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-md hover:bg-gray-100 hover:text-[#6576FF] focus:outline-none"
+                >
+                  Continue Shopping
+                </button>
+                <button
+                  className="py-2.5 px-5 text-sm font-medium text-white bg-[#6576FF] hover:bg-[#5d6ef6] rounded-md focus:outline-none"
+                  onClick={openModal}
+                >
+                  Checkout
+                </button>
+
+                {/* Checkout Modal */}
+                {isModalOpen && (
+                  <div className="fixed inset-0 bg-gray-500 bg-opacity-50 flex justify-center items-center">
+                    <div className="bg-white p-6 rounded-md w-4/5 md:w-1/2 lg:w-1/3 relative">
+                      <div className="flex justify-between items-center mb-4">
+                        <h2 className="text-xl font-semibold">Order Confirmation</h2>
+                        <button
+                          onClick={closeModal}
+                          className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 inline-flex justify-center items-center"
+                        >
+                          <svg className="w-3 h-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 14">
+                            <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"/>
+                          </svg>
+                          <span className="sr-only">Close modal</span>
+                        </button>
+                      </div>
+                      <p>Your order has been placed successfully!</p>
+                    </div>
+                  </div>
+                )}
+              </div>
+            </div>
+          </div>
+        </div>
       )}
-    </div>
     </>
   );
 }
